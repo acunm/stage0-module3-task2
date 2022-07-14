6411e84d237caae60cc834fb3d414644a56361cd
Acun <acun1993@gmail.com>
2022-07-14 17:11:07 +0300

    ugly names are changed 4

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,11 +3,16 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
+        int variableTwo = 2000;
         int value = 1000;
-        int value2 = 3000;
+        int variableThree = 4000;
+        int valueTwo = 3000;
         int variables = 5000;
         variables = 7000;
+        int valueThree = 90000;
+        int myInt = 100000;
         int myInteger = 1000000;
         int myVar = 1234567;
+        int myIntegerTwo = 1234567;
     }
 }
