1880001408bde09ed503349488536d4d27dc89c9
Acun <acun1993@gmail.com>
2022-07-14 17:00:45 +0300

    ugly names are changed

--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -6,9 +6,8 @@ public class JavaVariableNames {
         int money = 10;
         int hundred = 100;
         int back = 1000;
-        int _underscore = 10000;
+        int underscore = 10000;
         int sharp = 100000;
         int star = 1000000;
-        int BAD_EXAMPLE_OF_VAR_NAME = 1000000;
     }
 }
